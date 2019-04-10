955134f932b85d4ab197523afb04d3fd963cdd80
srup <sugiyama98i@gmail.com>
2019-04-11 08:17:46 +0900

    Support -p option

--- strace.c
+++ strace.c
@@ -6,24 +6,33 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "strace.h"
 
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
-		printf("Usage: ./strace [prog] [args]\n");
+		printf("Usage: ./strace PROG [ARGS] or ./strace -p PID\n");
 		exit(1);
 	}
 
-	pid_t pid = fork();
-	if (pid == -1) {
-		perror("Failed frok");
-		exit(1);
-	} else if (pid == 0) { // child
-		tracee(argv + 1);
+	if (strcmp(argv[1], "-p") == 0) {
+		pid_t tracee_pid = atoi(argv[2]);
+		if (ptrace(PTRACE_ATTACH, tracee_pid, NULL, NULL) < 0) {
+			perror("Failed ptrace(PTRACE_ATTACH, pid)");
+		}
+		tracer(tracee_pid);
 	} else {
-		tracer(pid);
+		pid_t pid = fork();
+		if (pid == -1) {
+			perror("Failed frok");
+			exit(1);
+		} else if (pid == 0) { // child
+			tracee(argv + 1);
+		} else {
+			tracer(pid);
+		}
 	}
 
 	return 0;
@@ -37,24 +46,24 @@ int tracee(char** cmd) {
 	return execvp(cmd[0], cmd);
 }
 
-int tracer(pid_t child_pid) {
+int tracer(pid_t tracee_pid) {
 	int status;
-	waitpid(child_pid, &status, 0); // wait for execvp
+	waitpid(tracee_pid, &status, 0); // wait for execvp
 
 	while (1) {
-		if (ptrace(PTRACE_SYSCALL, child_pid, NULL, NULL) < 0) {
+		if (ptrace(PTRACE_SYSCALL, tracee_pid, NULL, NULL) < 0) {
 			perror("Failed ptrace(PTRACE_SYSCALL");
 			exit(1);
 		}
 
 		int status;
-		waitpid(child_pid, &status, 0); // Wait for syscall enter or exit
+		waitpid(tracee_pid, &status, 0); // Wait for syscall enter or exit
 
 		if (WIFEXITED(status)) // Child terminated normally
 			break;
 
 		struct user_regs_struct regs;
-		if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) < 0) {
+		if (ptrace(PTRACE_GETREGS, tracee_pid, NULL, &regs) < 0) {
 			perror("Faild ptrace(PTRACE_GETREGS)");
 			exit(1);
 		}
